ca93b0bad0a999cc02b74826e7448f9eb81306d3
Jagadeesh <jagadeesh.cheluvaraj@gmail.com>
2017-05-11 07:52:30 +0530

    Updated D7_IIFE app.js file

--- D7_IIFE/Starter/app.js
+++ D7_IIFE/Starter/app.js
@@ -1,1 +1,10 @@
 // Your Javascript Code Goes Here
+var firstname = 'jane';
+
+(function (lastname) {
+    var firstname = 'john';
+    console.log(firstname + ' ' + lastname);
+}('doe'));
+
+console.log(firstname);
+
